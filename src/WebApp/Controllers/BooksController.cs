e47bed54a52eafee0c931863df90ffb958a6b334
TuanNH <tuannguyenhuy@vccorp.vn>
2024-07-15 20:48:21 +0700

    add detail, create

---
+++ src/WebApp/Controllers/BooksController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            Book book = new Book()
+            {
+                Id = 1,
+                Title = "Learning ASP.NET Core 2.0",
+                Genre = "Programming & Software Development",
+                Price = 45,
+                PublishDate = new System.DateTime(2012, 04, 23),
+                Authors = new List<string> { "Jason De Oliveira", "Michel Bruchet" }
+            };
+            return View(book);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to add the book to DB
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+    }
+}
